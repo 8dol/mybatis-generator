b789f4bde396f922471f4619749e2e205721e152
peace0phmind <peace0phmind@gmail.com>
2015-07-17 21:12:36 +0800

    add fields sql

---
+++ src/main/java/com/edol/generator/javamapper/EmptyJavaClientGenerator.java
@@ -0,0 +1,79 @@
+package com.edol.generator.javamapper;
+
+import com.edol.generator.xmlmapper.SimpleWithSqlMapperGenerator;
+import org.mybatis.generator.api.CommentGenerator;
+import org.mybatis.generator.api.dom.java.CompilationUnit;
+import org.mybatis.generator.api.dom.java.FullyQualifiedJavaType;
+import org.mybatis.generator.api.dom.java.Interface;
+import org.mybatis.generator.api.dom.java.JavaVisibility;
+import org.mybatis.generator.codegen.AbstractJavaClientGenerator;
+import org.mybatis.generator.codegen.AbstractXmlGenerator;
+import org.mybatis.generator.config.PropertyRegistry;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mybatis.generator.internal.util.StringUtility.stringHasValue;
+import static org.mybatis.generator.internal.util.messages.Messages.getString;
+
+/**
+ * Created by mind on 7/17/15.
+ */
+public class EmptyJavaClientGenerator extends AbstractJavaClientGenerator {
+
+    /**
+     *
+     */
+    public EmptyJavaClientGenerator() {
+        super(true);
+    }
+
+    @Override
+    public List<CompilationUnit> getCompilationUnits() {
+        progressCallback.startTask(getString("Progress.17", //$NON-NLS-1$
+                introspectedTable.getFullyQualifiedTable().toString()));
+        CommentGenerator commentGenerator = context.getCommentGenerator();
+
+        FullyQualifiedJavaType type = new FullyQualifiedJavaType(
+                introspectedTable.getMyBatis3JavaMapperType());
+        Interface interfaze = new Interface(type);
+        interfaze.setVisibility(JavaVisibility.PUBLIC);
+        commentGenerator.addJavaFileComment(interfaze);
+
+        String rootInterface = introspectedTable
+                .getTableConfigurationProperty(PropertyRegistry.ANY_ROOT_INTERFACE);
+        if (!stringHasValue(rootInterface)) {
+            rootInterface = context.getJavaClientGeneratorConfiguration()
+                    .getProperty(PropertyRegistry.ANY_ROOT_INTERFACE);
+        }
+
+        if (stringHasValue(rootInterface)) {
+            FullyQualifiedJavaType fqjt = new FullyQualifiedJavaType(
+                    rootInterface);
+            interfaze.addSuperInterface(fqjt);
+            interfaze.addImportedType(fqjt);
+        }
+
+        List<CompilationUnit> answer = new ArrayList<CompilationUnit>();
+        if (context.getPlugins().clientGenerated(interfaze, null,
+                introspectedTable)) {
+            answer.add(interfaze);
+        }
+
+        List<CompilationUnit> extraCompilationUnits = getExtraCompilationUnits();
+        if (extraCompilationUnits != null) {
+            answer.addAll(extraCompilationUnits);
+        }
+
+        return answer;
+    }
+
+    public List<CompilationUnit> getExtraCompilationUnits() {
+        return null;
+    }
+
+    @Override
+    public AbstractXmlGenerator getMatchedXMLGenerator() {
+        return new SimpleWithSqlMapperGenerator();
+    }
+}
---
+++ src/main/java/com/edol/generator/xmlmapper/elements/SimpleFieldsSqlElementGenerator.java
@@ -0,0 +1,66 @@
+package com.edol.generator.xmlmapper.elements;
+
+import org.mybatis.generator.api.IntrospectedColumn;
+import org.mybatis.generator.api.dom.xml.Attribute;
+import org.mybatis.generator.api.dom.xml.TextElement;
+import org.mybatis.generator.api.dom.xml.XmlElement;
+import org.mybatis.generator.codegen.mybatis3.MyBatis3FormattingUtilities;
+import org.mybatis.generator.codegen.mybatis3.xmlmapper.elements.AbstractXmlElementGenerator;
+import org.mybatis.generator.config.PropertyRegistry;
+import org.mybatis.generator.internal.util.StringUtility;
+
+import java.util.Iterator;
+
+/**
+ * Created by mind on 7/17/15.
+ */
+public class SimpleFieldsSqlElementGenerator extends AbstractXmlElementGenerator {
+
+    public SimpleFieldsSqlElementGenerator() {
+        super();
+    }
+
+    @Override
+    public void addElements(XmlElement parentElement) {
+        XmlElement answer = new XmlElement("sql"); //$NON-NLS-1$
+
+        answer.addAttribute(new Attribute("id", "all_fields"));
+
+        context.getCommentGenerator().addComment(answer);
+
+        StringBuilder sb = new StringBuilder();
+        Iterator<IntrospectedColumn> iter = introspectedTable.getAllColumns()
+                .iterator();
+        while (iter.hasNext()) {
+            sb.append(MyBatis3FormattingUtilities.getSelectListPhrase(iter
+                    .next()));
+
+            if (iter.hasNext()) {
+                sb.append(", "); //$NON-NLS-1$
+            }
+
+            if (sb.length() > 80) {
+                answer.addElement(new TextElement(sb.toString()));
+                sb.setLength(0);
+            }
+        }
+
+        if (sb.length() > 0) {
+            answer.addElement((new TextElement(sb.toString())));
+        }
+
+        String orderByClause = introspectedTable.getTableConfigurationProperty(PropertyRegistry.TABLE_SELECT_ALL_ORDER_BY_CLAUSE);
+        boolean hasOrderBy = StringUtility.stringHasValue(orderByClause);
+        if (hasOrderBy) {
+            sb.setLength(0);
+            sb.append("order by "); //$NON-NLS-1$
+            sb.append(orderByClause);
+            answer.addElement(new TextElement(sb.toString()));
+        }
+
+        if (context.getPlugins().sqlMapSelectAllElementGenerated(
+                answer, introspectedTable)) {
+            parentElement.addElement(answer);
+        }
+    }
+}
